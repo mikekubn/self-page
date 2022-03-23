3cdff3da4391653f5446c458a6f9e0d2ec14fb96
Michael Kubín <Michael.kubin@livesporttv.cz>
2022-03-23 11:36:12 +0100

    add: framer motion notification

--- src/components/Notification/index.tsx
+++ src/components/Notification/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
+import { AnimatePresence, motion } from 'framer-motion';
 import { INotificaiton } from '@/hooks/useNotification';
 
 interface INotificaitonBox {
-  display: string,
+  display: boolean,
   note: string,
   img: string,
 }
@@ -11,17 +12,15 @@ interface INotificaitonBox {
 const Notification = (props: INotificaiton): React.ReactElement | null => {
   const { visible, status, note } = props;
 
-  const display = visible ? 'flex' : 'hidden';
-
   if (status === 'success') {
     return (
-      <NotificaitonBox display={display} note={note} img="/img/success.png" />
+      <NotificaitonBox display={visible} note={note} img="/img/success.png" />
     );
   }
 
   if (status === 'error') {
     return (
-      <NotificaitonBox display={display} note={note} img="/img/error.png" />
+      <NotificaitonBox display={visible} note={note} img="/img/error.png" />
     );
   }
 
@@ -29,12 +28,26 @@ const Notification = (props: INotificaiton): React.ReactElement | null => {
 };
 
 const NotificaitonBox = ({ display, img, note }: INotificaitonBox) => (
-  <div className={`z-50 ${display} justify-end px-7 animate-move`}>
-    <div className="flex items-center px-4 mt-20 rounded-lg w-60 h-11 bg-sky500/5">
-      <Image src={img} width={18} height={18} />
-      <p className="pl-4">{note}</p>
-    </div>
-  </div>
+  <AnimatePresence>
+    {
+      display ? (
+        <motion.div
+          key="notification"
+          initial={{ x: 300, opacity: 0 }}
+          animate={{ x: -28, opacity: 1 }}
+          exit={{ x: 300, opacity: 0 }}
+          transition={{ duration: 0.7, ease: 'easeInOut' }}
+          className="z-50 flex justify-end mt-20"
+        >
+          <div className="flex items-center px-4 rounded-lg w-60 h-11 bg-sky500/5">
+            <Image src={img} width={18} height={18} />
+            <p className="pl-4">{note}</p>
+          </div>
+        </motion.div>
+      )
+        : null
+    }
+  </AnimatePresence>
 );
 
 export default Notification;
