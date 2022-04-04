57acc5cb72a9a72a858a665bde7f75d71debe877
Michael Kubín <Michael.kubin@livesporttv.cz>
2022-04-04 20:35:24 +0200

    add: top arrow button

--- src/components/TopPanel/index.tsx
+++ src/components/TopPanel/index.tsx
@@ -5,7 +5,7 @@ import ToggleButton from '@/components/ToggleButton';
 import { useScroll } from '@/hooks/index';
 
 const TopPanel = (): React.ReactElement => {
-  const { move } = useScroll();
+  const { move, num } = useScroll();
   const { state, toggle } = useThemeProvider();
 
   const display = move === 'down' ? 'hidden' : 'flex';
@@ -13,6 +13,7 @@ const TopPanel = (): React.ReactElement => {
   return (
     <header className={`fixed top-0 left-0 z-50 ${display} flex-row items-center w-full px-7 h-14`}>
       <div className="flex ml-auto">
+        {num !== 0 ? <BackToTop /> : null}
         <RenderThemeImage val={state} />
         <ToggleButton value={state} callback={(val) => toggle(val)} />
       </div>
@@ -22,10 +23,16 @@ const TopPanel = (): React.ReactElement => {
 
 export default TopPanel;
 
+const BackToTop = () => (
+  <div className="flex flex-row items-center p-2 cursor-pointer">
+    <Image src="/img/top-arrow.png" height={30} width={30} onClick={() => window.scrollTo(0, 0)} />
+  </div>
+);
+
 const ProvideImage = ({ val }: { val: boolean }) => {
   if (val) {
     return (
-      <Image src="/img/moon.png" height={30} width={30} />
+      <Image src="/img/moon.png" height={26} width={26} />
     );
   }
 
